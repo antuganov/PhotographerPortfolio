bdde40bd4768dd03d00b754986b1d4e24a4a811e
Lev <antgnff@gmail.com>
2024-09-08 15:17:58 +0400

    Migration from SQLite to PostgreSQL

--- delevoper/settings.py
+++ delevoper/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "delevoper.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photographer_portfolio',
+        'USER': 'portfolio_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
